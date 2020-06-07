5e251b770161287c07f5a59f5aa8e508929fd9f7
carlcc <carlmarxchen@foxmail.com>
2020-06-07 17:05:34 +0800

    Fix warning

--- Loong/LoongAsset/src/LoongAsset/LoongImage.cpp
+++ Loong/LoongAsset/src/LoongAsset/LoongImage.cpp
@@ -6,10 +6,20 @@
 #include "LoongFileSystem/LoongFileSystem.h"
 #include "LoongFoundation/LoongLogger.h"
 
+#ifdef _MSC_VER
+#pragma warning(push)
+// e.g. This function or variable may be unsafe. Consider using fopen_s instead. 
+#pragma warning(disable : 4996)
+#endif
+
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image.h"
 #include "stb_image_write.h"
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
 
 namespace Loong::Asset {
 
@@ -24,7 +34,7 @@ LoongImage::LoongImage(const std::string& path)
     assert(LoongFileSystem::LoadFileContent(path, buffer_, fileSize) == fileSize);
 
     LOONG_TRACE("Load image '{}' to '0x{:0X}'", path, (void*)this);
-    buffer_ = reinterpret_cast<char*>(stbi_load_from_memory(buffer.data(), fileSize, &width_, &height_, &channelCount_, 0));
+    buffer_ = reinterpret_cast<char*>(stbi_load_from_memory(buffer.data(), int(fileSize), &width_, &height_, &channelCount_, 0));
     if (buffer_ == nullptr) {
         LOONG_ERROR("Failed to load image '{}'", path);
         width_ = 0;
